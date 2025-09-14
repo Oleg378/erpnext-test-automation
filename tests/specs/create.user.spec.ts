3003d61c0efd688248a9d519d5ad56a8bd5452ed
Oleg <oleg-dmitriewich@yandex.ru>
2025-09-14 23:43:40 +0300

    Demo with @tags + data driven test;

--- tests/specs/create.user.spec.ts
+++ tests/specs/create.user.spec.ts
@@ -1,12 +1,13 @@
 import {apiTest} from '../fixtures/api.test.fixture';
 import {ApiClient} from '../app/api/ApiClient';
-import {ProfileRole, ProfileRoles} from '../tools/ProfileRoles';
+import {ProfileRoles} from '../tools/ProfileRoles';
 
-apiTest.describe('Create new user', () => {
-    apiTest('Create a new User with role Accounts', async ({apiManager}) => {
-        const profileRole: ProfileRole = ProfileRoles.Accounts;
-        await ApiClient.postRetrieveAdminCookies(apiManager);
-        const responseData = await ApiClient.postCreateNewUser(apiManager, profileRole);
-        await apiManager.attachDataToReport(`New user with profileRole ${profileRole.role_profile_name} has been created!`, responseData);
-    })
+apiTest.describe('Create new users with different roles: @sanity @api', () => {
+    Object.values(ProfileRoles).forEach(role => {
+        apiTest(`A new User with role ${role.role_profile_name}`, async ({ apiManager }) => {
+            await ApiClient.postRetrieveAdminCookies(apiManager);
+            const username: string = await ApiClient.postCreateNewUser(apiManager, role);
+            await apiManager.attachDataToReport(`New user with profileRole ${role.role_profile_name} has been created!`, username);
+        });
+    });
 })
--- tests/specs/login.spec.ts
+++ tests/specs/login.spec.ts
@@ -5,24 +5,24 @@ import {PayablesPage} from '../app/pages/navigation/PayablesPage';
 import {test} from '../fixtures/combined.test.fixture';
 
 test.beforeAll(async ({apiManager}) => {
-    let testGet = await apiManager.get('/');
+    const testGet = await apiManager.get('/', 'sanity check', false);
     await apiManager.expectResponseToBeOk(testGet);
 })
 
-test.describe('API + UI DEMO:', () => {
-    test('API DEMO: ping base url', async ({apiManager}) => {
+test.describe('API + UI DEMO: @sanity', () => {
+    test('API DEMO: Get a Customer list @api', async ({apiManager}) => {
         let data = {
             'usr': 'Administrator',
             'pwd': 'admin'
         }
         await apiManager.post('/api/method/login', data);
-        let testGet = await apiManager.get('/api/resource/Customer');
+        const testGet = await apiManager.get('/api/resource/Customer');
         await apiManager.expectResponseToBeOk(testGet);
     })
 
     test('IU DEMO: Login as admin', async ({ pageManager }) => {
-        let homePage: HomePage = await new LoginPage(pageManager).loginAsAdmin();
-        let payablesPage: PayablesPage = await homePage.navigateTo(Navigation.ACCOUNTING);
+        const homePage: HomePage = await new LoginPage(pageManager).loginAsAdmin();
+        const payablesPage: PayablesPage = await homePage.navigateTo(Navigation.ACCOUNTING);
         await payablesPage.navigateTo(Navigation.STOCK);
     });
 })
