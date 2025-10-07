eb36363625f733e12c5e26c028ab12a3b659072d
Oleg <oleg-dmitriewich@yandex.ru>
2025-10-07 22:47:19 +0300

    simplified initialisation: SalesFlow.create() impl;

--- tests/app/flows/SalesFlow.ts
+++ tests/app/flows/SalesFlow.ts
@@ -6,23 +6,56 @@ import {LogInUtils} from '../../tools/utils/LogInUtils';
 import {ProfileRoles} from '../../tools/ProfileRoles';
 import {HomePage} from '../pages/navigation/HomePage';
 import {Navigation} from '../components/Navigation';
+import {TestDataFactory} from '../../tools/utils/TestDataFactory';
+import {ItemGroupEnum} from '../../tools/utils/enums/ItemGroupEnum';
+import {UOMEnum} from '../../tools/utils/enums/UOMEnum';
+
+interface SalesFlowConfig {
+    apiManager: ApiManager;
+    items?: Item[];
+    supplier?: Supplier;
+    customer?: Customer;
+}
 
 export class SalesFlow {
-    private readonly DATA_UTILS: DataUtils;
-    private readonly SALES_USERNAME: string = 'Sales_User';
+    private readonly dataUtils: DataUtils;
+    private readonly items: Item[];
+    private readonly supplier: Supplier;
+    private readonly customer: Customer;
+    private readonly apiManager: ApiManager;
+
+    private static readonly SALES_USERNAME: string = 'Sales_User';
+    private static readonly DEFAULT_ITEMS: Item[] = [
+        TestDataFactory.generateItemInfo(ItemGroupEnum.PRODUCTS, UOMEnum.UNIT, 'THE-RING'),
+        TestDataFactory.generateItemInfo(ItemGroupEnum.PRODUCTS, UOMEnum.KG, 'AXE'),
+        TestDataFactory.generateItemInfo(ItemGroupEnum.PRODUCTS, UOMEnum.BOX, 'ARROW-FOR-BOROMIR')
+    ];
+    private static readonly DEFAULT_SUPPLIER: Supplier = {supplier_name: 'SARUMAN'};
+    private static readonly DEFAULT_CUSTOMER: Customer = {
+        customer_name: 'BALROG',
+        customer_type: 'Company'
+    };
 
     constructor(
-        private readonly items: Item[],
-        private readonly supplier: Supplier,
-        private readonly customer: Customer,
-        private readonly apiManager: ApiManager
+        config: SalesFlowConfig
     ) {
-        this.DATA_UTILS = new DataUtils(apiManager);
+        this.apiManager = config.apiManager;
+        this.dataUtils = new DataUtils(config.apiManager);
+        this.items = config.items || SalesFlow.DEFAULT_ITEMS;
+        this.supplier = config.supplier || SalesFlow.DEFAULT_SUPPLIER;
+        this.customer = config.customer || SalesFlow.DEFAULT_CUSTOMER;
+
     }
 
     async init() {
-        await this.DATA_UTILS.ensureItemsWithPricingAndSupplier(this.items, this.supplier, false);
-        await this.DATA_UTILS.ensureCustomerExists(this.customer, false);
+        await this.dataUtils.ensureItemsWithPricingAndSupplier(this.items, this.supplier, false);
+        await this.dataUtils.ensureCustomerExists(this.customer, false);
+    }
+
+    static async create(config: SalesFlowConfig): Promise<SalesFlow> {
+        const instance = new SalesFlow(config);
+        await instance.init();
+        return instance;
     }
 
     sales = {
@@ -33,7 +66,7 @@ export class SalesFlow {
                 this.apiManager,
                 pageManager,
                 ProfileRoles.Sales,
-                this.SALES_USERNAME
+                SalesFlow.SALES_USERNAME
             )).homePage;
             await homePage.navigateTo(Navigation.SELLING)
             // POM magic
